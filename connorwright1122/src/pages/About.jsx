b25c18bbea49f011ecde88763ea22a786e5acd85
connorwright1122 <connorwright1122@gmail.com>
2025-05-06 16:11:26 -0400

    About Page

--- connorwright1122/src/pages/About.jsx
+++ connorwright1122/src/pages/About.jsx
@@ -16,10 +16,12 @@ export default function About() {
             <div class="site-body">
                 <p>Hi, I'm Connor! I'm a fourth year Computer Science student at Georgia Tech with concentrations in
                     Artificial Intelligence and Computer Graphics / Media. </p>
-                <p>I've worked for Georgia Tech's Expressive Machinery Lab developing an embodied co-cretive AI improv dance agent and GT OIT's Academic / Research Technology Division researching use cases for new VR softwares to be implemented into GT courses. 
-                    For the future, I'm seeking experience in software engineering and product management. </p>
-                <p>In my free time, I enjoy photography, 3D modeling, game development, and watching weird arthouse films. You can find some of my work in the Extras tab.</p>
-                
+                <p>I will be graduating in Spring 2026 and am seeking software engineering or technical artist roles. You can find my past experience and projects on the Portfolio tab.</p>
+                <p>I am fascinated by the intersection of art and technology and how we can use emerging technologies such as XR and AI tools to enhance human creativity. 
+                    In my spare time you can find me building tools for artists or working on my own endless stream of passion projects. 
+                    I also love everything to do with film and VFX - keep an eye out for my short film Tachyon Deliverance coming soon to a galaxy near you (email me to get the screening link).
+                    Additionally, I dabble in photography, 3D modeling, hiking, and game development. 
+                    You can find some of my work on the Extras page.</p>
             </div>
             
             <a href="/assets/documents/ConnorWrightResume.pdf" target="_blank">
