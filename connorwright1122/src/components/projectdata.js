fc446f1040df8c7866798567b6ed9b151865afde
connorwright1122 <connorwright1122@gmail.com>
2025-05-06 18:29:27 -0400

    S24-S25 projects

--- connorwright1122/src/components/projectdata.js
+++ connorwright1122/src/components/projectdata.js
@@ -2,38 +2,216 @@ export const experienceData = [
     {
         title: 'Software Engineering Intern',
         img: '/assets/images/experience/hirez-logo.jpeg',
-        link: '#',
+        //link: '#',
         details: {
             origin: 'Hi-Rez Studios',
             date: 'Summer 2024 - Spring 2025',
-            tools: 'Stable Diffusion, Python, \nInvokeAI, PyTorch, Unreal Engine'
+            tools: 'Stable Diffusion, Python, InvokeAI, PyTorch, Unreal Engine'
         },
     },
 
     {
-        title: 'Software Engineering Intern',
-        img: '/assets/images/experience/hirez-logo.jpeg',
-        link: '#',
+        title: 'Product Management Intern',
+        img: '/assets/images/experience/thd-logo.jpeg',
+        //link: '#',
         details: {
-            origin: 'Hi-Rez Studios',
-            date: 'Summer 2024 - Spring 2025',
-            tools: 'Stable Diffusion, Python, \nInvokeAI, PyTorch, Unreal Engine'
+            origin: 'The Home Depot',
+            date: 'Summer 2024',
+            tools: 'Figma, Photoshop'
         },
     },
+
+    {
+        title: 'Undergraduate Researcher',
+        img: '/assets/images/experience/gt-expressive.jpg',
+        link: 'https://expressivemachinery.gatech.edu/projects/luminai/',
+        details: {
+            origin: 'Expressive Machinery Lab',
+            date: 'Spring 2024',
+            tools: 'C#, NoSQL, MongoDB, Unity, Blender'
+        },
+    },
+
+    {
+        title: 'Undergraduate Researcher',
+        img: '/assets/images/experience/gt-vip.jpeg',
+        //link: 'https://expressivemachinery.gatech.edu/projects/luminai/',
+        details: {
+            origin: 'GT VIP - Unlocking and Analyzing Historical Texts',
+            date: 'Spring 2024 - Spring 2025',
+            tools: 'C#, NoSQL, MongoDB, Unity, Blender'
+        },
+    },
+
+    {
+        title: 'VR Software Developer',
+        img: '/assets/images/experience/gt-oit-omniverse.jpeg',
+        //link: 'https://expressivemachinery.gatech.edu/projects/luminai/',
+        details: {
+            origin: 'GT OIT - Academic/Research Technology Division',
+            date: 'Spring 2023 - Spring 2024',
+            tools: 'C#, Unity, NVIDIA Omniverse, LumaAI, Blender, Figma'
+        },
+    },
+
+    {
+        title: 'Game Development Extern',
+        img: '/assets/images/experience/shipwright-studios-1710.avif',
+        //link: 'https://expressivemachinery.gatech.edu/projects/luminai/',
+        details: {
+            origin: 'Shipwright Studios',
+            date: 'Winter 2022',
+            tools: 'C#, Unity, Substance Painter, Blender, Figma'
+        },
+    },
+
+
     
-    // ...other entries
   ];
   
   export const projectData = [
     {
-      title: 'anim2spritesheet',
-      img: '/assets/images/projects/anim2spritesheet-cover-2.jpg',
-      origin: 'Blender Addon',
-      date: 'Summer 2024',
-      tools: 'Python, Blender API, PIL',
-      description: 'Automatic rendering of 3D models as 2D spritesheets',
-      link: 'https://github.com/connorwright1122/anim2spritesheet',
+        title: 'Buzz Studios Film Archive',
+        //img: '/assets/images/projects/anim2spritesheet-cover-2.jpg',
+        link: 'https://github.com/Buzz-Studios-Development-Team/BuzzStudiosFilmArchive',
+        details: {
+            origin: 'Buzz Studios Filmmaking Club',
+            date: 'Spring 2025 - Present',
+            tools: 'React.js, Node.js, GCP, HTML/CSS/JS',
+            description: 'Developer for film club\'s in-house short film streaming service',
+        }
     },
-    // ...other projects
+
+    {
+        title: 'VR Heartrate State Analysis',
+        //img: '/assets/images/projects/anim2spritesheet-cover-2.jpg',
+        //link: 'https://github.com/connorwright1122/anim2spritesheet',
+        details: {
+            origin: 'VR @ GT / Big Data Big Impact Club',
+            date: 'Spring 2025 - Present',
+            tools: 'C#, Unity, Python/PyTorch, ADB',
+            description: 'ML-based prediction of heartrate via micro-movements of VR headset',
+        }
+    },
+
+    {
+        title: '3D Character to 2D AI Imagery Pipeline',
+        //img: '/assets/images/projects/anim2spritesheet-cover-2.jpg',
+        link: 'https://github.gatech.edu/pages/riyer75/4641/',
+        details: {
+            origin: 'Hi-Rez Studios',
+            date: 'Summer 2024 - Spring 2025',
+            tools: 'Unreal Engine, Python/PyTorch/Pandas, InvokeAI',
+            description: 'AI-powered tools and workflows for dataset generation, model training, and AI-generated image segmentation',
+        }
+    },
+
+    {
+        title: 'ML Inland Water Quality Predictor',
+        //img: '/assets/images/projects/anim2spritesheet-cover-2.jpg',
+        link: 'https://github.gatech.edu/pages/riyer75/4641/',
+        details: {
+            origin: 'CS 4641 - Machine Learning - Semester-Long Project',
+            date: 'Spring 2025',
+            tools: 'Python/PyTorch/Pandas',
+            description: 'ML-based estimation of inland water quality using satellite imagery reflectance data',
+        }
+    },
+
+    {
+        title: 'VTT Caption AI Translator',
+        //img: '/assets/images/projects/anim2spritesheet-cover-2.jpg',
+        link: 'https://github.com/Buzz-Studios-Development-Team/vtt-ai-translator',
+        details: {
+            origin: 'Buzz Studios Filmmaking Club',
+            date: 'Spring 2025',
+            tools: 'Python, OpenAI API',
+            description: 'Fluent translation of VTT subtitle files using OpenAI API',
+        }
+    },
+
+    {
+        title: 'Tachyon Deliverance',
+        //img: '/assets/images/projects/anim2spritesheet-cover-2.jpg',
+        //link: 'https://github.com/connorwright1122/anim2spritesheet',
+        details: {
+            origin: 'Buzz Studios Filmmaking Club',
+            date: 'Spring 2025',
+            tools: 'Blender, DaVinci Resolve, Photoshop, Figma, InvokeAI, TRELLIS',
+            description: 'Director/Writer/Editor/VFX Artist/Prop Master for Sci-Fi Comedy short film with 20+ VFX shots',
+        }
+    },
+
+    {
+        title: 'Custom Computer Graphics Scenes',
+        //img: '/assets/images/projects/anim2spritesheet-cover-2.jpg',
+        //link: 'https://github.com/connorwright1122/anim2spritesheet',
+        details: {
+            origin: 'CS 3451 - Computer Graphics',
+            date: 'Fall 2024',
+            tools: 'OpenGL, C++, GLSL',
+            description: 'Custom implementations of CG algorithms during CS 3451',
+        }
+    },
+
+    {
+        title: 'anim2spritesheet',
+        img: '/assets/images/projects/anim2spritesheet-cover-2.jpg',
+        link: 'https://github.com/connorwright1122/anim2spritesheet',
+        details: {
+            origin: 'Blender Addon',
+            date: 'Summer 2024',
+            tools: 'Python, Blender API, PIL',
+            description: 'Automatic rendering of 3D animations as 2D albedo, normal, or emission spritesheets',
+        }
+    },
+
+    {
+        title: 'Invoke AI Prompt Builder',
+        //img: '/assets/images/projects/anim2spritesheet-cover-2.jpg',
+        link: 'https://github.com/connorwright1122/anim2spritesheet',
+        details: {
+            origin: 'Product @ GT / Invoke AI Collaboration',
+            date: 'Spring 2024',
+            tools: 'InvokeAI, Figma, Photoshop',
+            description: 'Proposed redesign / features for prompt-building experience of Invoke AI image generator',
+        }
+    },
+
+    {
+        title: 'LuminAI Research / Developer Tools',
+        //img: '/assets/images/projects/anim2spritesheet-cover-2.jpg',
+        link: 'https://github.com/connorwright1122/anim2spritesheet',
+        details: {
+            origin: 'Expressive Machinery Lab',
+            date: 'Spring 2024',
+            tools: 'C#, Unity, NoSQL, MongoDB, Blender',
+            description: 'Tools for LuminAI developers/researchers - gesture database visualizer, performance manager UI, etc.',
+        }
+    },
+
+    {
+        title: 'GT OIT XR Projects',
+        //img: '/assets/images/projects/anim2spritesheet-cover-2.jpg',
+        //link: 'https://github.com/connorwright1122/anim2spritesheet',
+        details: {
+            origin: 'GT OIT Academic/Research Technology Division',
+            date: 'Spring 2024',
+            tools: 'C#, Unity, Blender, LumaAI, NVIDIA Omniverse',
+            description: 'Cross-organizational XR projects for virtual tours, simulations, photogrammetry, and digital twins',
+        }
+    },
+
+    {
+        title: 'Dun Dun Dungeon',
+        //img: '/assets/images/projects/anim2spritesheet-cover-2.jpg',
+        //link: 'https://github.com/connorwright1122/anim2spritesheet',
+        details: {
+            origin: 'CS 2340 - Semester-Long Project',
+            date: 'Fall 2023',
+            tools: 'Java, Android Studio',
+            description: 'Android game utilizing MVC architecture and design patterns',
+        }
+    },
   ];
   
