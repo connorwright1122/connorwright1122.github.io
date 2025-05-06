38839df6d249bde65a04caab755e4ab52b373da4
connorwright1122 <connorwright1122@gmail.com>
2025-05-06 00:55:10 -0400

    Basic home page

--- connorwright1122/src/App.js
+++ connorwright1122/src/App.js
@@ -1,9 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home'
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/*
+        <Route path="/about" element={<About />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/extras" element={<Extras />} />
+        */}
+      </Routes>
       <h1>Hello</h1>
     </div>
   );
---
+++ connorwright1122/src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import {Link} from 'react-router-dom'
+
+export default function Navbar() {
+    return(
+        <header>
+            <div class="nav-v2">
+                <h1>Connor Wright</h1>
+                <ul class="navigation">
+                    <li>
+                        <Link to="./pages/Home">Home</Link>
+                    </li>
+                    {/*
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/portfolio">Portfolio</Link>
+                    </li>
+                    <li>
+                        <Link to="/extras">Extras</Link>
+                    </li>
+                    */}
+                </ul>
+            </div>
+        </header>
+    );
+}
